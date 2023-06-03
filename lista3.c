1ac7dce827e08e6e60d6b16d10d760f1511b5b95
MozanielJr <mozaniel.junior@a.ucb.br>
2023-06-03 10:21:54 -0300

    atualizacaoinicial

---
+++ lista3.c
@@ -0,0 +1,18 @@
+// Lista 3
+// Elabore um programa onde o usuário armazenará por meio de Structs os dados de "Produtos" 
+// e "Fabricantes", serão cadastrados no mínimo 2 fabricantes (máximo 5) e no mínimo 5 produtos 
+// (máximo 50).
+// Produto: Descrição, peso, valor de compra, valor de venda, valor do lucro (calculado 
+// automaticamente), percentual do lucro (calculado automaticamente) e fabricante.
+// Fabricante: Marca (nome), site, telefone, UF (todas as unidades da federação previamente 
+// cadastradas AC, AM, AP,.., SP, TO).
+// Por fim crie um menu para emitir os relatórios abaixo:
+// - Utilizem o máximo de funções e procedimentos no estilo "le_valida" para minimizar os erros 
+// de cadastros por parte dos usuários.
+// Façam bom uso de "tabelas" para apresentação das telas como no exemplo abaixo:
+// - O trabalho deverá ser desenvolvido em grupos de até 5 integrantes.
+// - Obrigatoriamente os códigos da entrega 3 serão compartilhados pelo github.com . 
+// - Somente 1 integrante do grupo deverá enviar a atividade pelo AVA (arquivo .txt contendo 
+// os nomes dos integrantes e link do github).
+// - Para cada envio de outros integrantes no AVA será descontado 1 ponto do grupo.
+// - Entrega 3 – 05/06
